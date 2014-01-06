883719171d9e51f87975f07e4908cbd56aa3e363
shadeglare <shadeglare@gmail.com>
2014-01-06 17:24:57 +0400

    made stream message editable

--- FrontEnd/templates/messenger.js
+++ FrontEnd/templates/messenger.js
@@ -4,6 +4,23 @@ window.onload = function() {
     var streamWrapElem = streamElem.getElementsByClassName("wrap")[0];
     var composerElem = document.getElementById("composer");
 
+    var checkElemOverflow = function(elem) {
+        elem.style.overflow = "auto";
+
+        var scrollHeight = elem.scrollHeight;
+        var scrollWidth = elem.scrollWidth;
+
+        var clientHeight = elem.clientHeight;
+        var clientWidth = elem.clientWidth;
+
+        var isOverflowX = scrollWidth > clientWidth;
+        var isOverflowY = scrollHeight > clientHeight;
+        var isOverflow = isOverflowX || isOverflowY;
+
+        elem.style.overflow = "hidden";
+
+        return isOverflow;
+    };
     var clearMessageElem = function(messageElem) {
         var editorElem = messageElem.getElementsByClassName("editor")[0];
         editorElem.innerHTML = "";
@@ -23,18 +40,83 @@ window.onload = function() {
     var appendMessageElem = function(messageElem) {
         streamWrapElem.appendChild(messageElem);
     };
+    var checkMessageElemOverflow = function(messageElem) {
+        var containerElem = messageElem.getElementsByClassName("container")[0];
+        var isOverflow = checkElemOverflow(containerElem);
+        if (isOverflow) {
+            containerElem.style["outline"] = "2px solid #fffa94";
+        } else {
+            containerElem.style["outline"] = "2px solid #fff";
+        }
+    };
+    var isEditingMessageElem = function(messageElem) {
+        var containerElems = messageElem.getElementsByClassName("container dynamic");
+        return containerElems.length !== 0;
+    };
+    var beginEditingMessageElem = function(messageElem) {
+        var editElem = messageElem.getElementsByClassName("edit")[0];
+        var clearElem = messageElem.getElementsByClassName("clear")[0];
+        var shareElem = messageElem.getElementsByClassName("share")[0];
+        var fullscreenElem = messageElem.getElementsByClassName("fullscreen")[0];
+        var deleteElem = messageElem.getElementsByClassName("delete")[0];
+
+        var containerElem = messageElem.getElementsByClassName("container")[0];
+        var editorElem = messageElem.getElementsByClassName("editor")[0];
+
+        editElem.innerText = "finish";
+        clearElem.style["display"] = "block";
+        shareElem.style["display"] = "none";
+        fullscreenElem.style["display"] = "none";
+        deleteElem.style["display"] = "none";
+
+        messageElem.className = "message dynamic";
+        containerElem.className = "container dynamic";
+        containerElem.style["overflow"] = "auto";
+        containerElem.style["outline"] = "2px solid #eee";
+        editorElem.setAttribute("contenteditable", "true");
+    };
+    var endEditingMessageElem = function(messageElem) {
+        var editElem = messageElem.getElementsByClassName("edit")[0];
+        var clearElem = messageElem.getElementsByClassName("clear")[0];
+        var shareElem = messageElem.getElementsByClassName("share")[0];
+        var fullscreenElem = messageElem.getElementsByClassName("fullscreen")[0];
+        var deleteElem = messageElem.getElementsByClassName("delete")[0];
+
+        var containerElem = messageElem.getElementsByClassName("container")[0];
+        var editorElem = messageElem.getElementsByClassName("editor")[0];
+
+        editElem.innerText = "edit";
+        clearElem.style["display"] = "none";
+        shareElem.style["display"] = "block";
+        fullscreenElem.style["display"] = "block";
+        deleteElem.style["display"] = "block";
+
+        messageElem.className = "message static";
+        containerElem.className = "container static";
+        containerElem.style["overflow"] = "hidden";
+        editorElem.setAttribute("contenteditable", "false");
+    };
     var scrollToBottom = function(elem) {
         elem.scrollTop = elem.scrollHeight;
     };
 
     var imbueStreamMessageElem = function(messageElem) {
         var deleteElem = messageElem.getElementsByClassName("delete")[0];
+        var clearElem = messageElem.getElementsByClassName("clear")[0];
         var editElem = messageElem.getElementsByClassName("edit")[0];
         var fullscreenElem = messageElem.getElementsByClassName("fullscreen")[0];
         var shareElem = messageElem.getElementsByClassName("share")[0];
 
         var editElemHandler = function() {
-            alert("Not implemented");
+            if (isEditingMessageElem(messageElem)) {
+                endEditingMessageElem(messageElem);
+                checkMessageElemOverflow(messageElem);
+            } else {
+                beginEditingMessageElem(messageElem);
+            }
+        };
+        var clearElemHandler = function() {
+            clearMessageElem(messageElem);
         };
         var fullscreenElemHandler = function() {
             alert("Not implemented");
@@ -44,6 +126,7 @@ window.onload = function() {
         };
         var deleteElemHandler = function() {
             editElem.removeEventListener("click", editElemHandler);
+            clearElem.removeEventListener("click", clearElemHandler);
             fullscreenElem.removeEventListener("click", fullscreenElemHandler);
             shareElem.removeEventListener("click", shareElemHandler);
             deleteElem.removeEventListener("click", deleteElemHandler);
@@ -51,9 +134,12 @@ window.onload = function() {
         };
 
         editElem.addEventListener("click", editElemHandler);
+        clearElem.addEventListener("click", clearElemHandler);
         fullscreenElem.addEventListener("click", fullscreenElemHandler);
         shareElem.addEventListener("click", shareElemHandler);
         deleteElem.addEventListener("click", deleteElemHandler);
+
+        checkMessageElemOverflow(messageElem);
     };
     var imbueComposerMessageElem = function(messageElem) {
         var sendElem = messageElem.getElementsByClassName("send")[0];
@@ -66,6 +152,7 @@ window.onload = function() {
             imbueStreamMessageElem(newMessageElem);
             appendMessageElem(newMessageElem);
             scrollToBottom(streamWrapElem);
+            checkMessageElemOverflow(newMessageElem);
             clearMessageElem(messageElem);
         });
         clearElem.addEventListener("click", function() {
@@ -89,10 +176,9 @@ window.onload = function() {
     initializeComposerMessageElem();
 
     // AddHoc
-
     var hideComposerElem = document.getElementById("hide-composer");
     hideComposerElem.addEventListener("click", function() {
-        var computedStyle = window.getComputedStyle(composerElem);
+        var computedStyle = window.getComputedStyle(composerElem, null);
         if (computedStyle["display"] === "table-row") {
             composerElem.style["display"] = "none";
         } else {
