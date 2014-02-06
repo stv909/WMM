51fb5cd88fbb97829540f33c6657674cd3ff326c
shadeglare <shadeglare@gmail.com>
2014-02-06 23:14:25 +0400

    added event module and tests

---
+++ FrontEnd/templates/event-tests.js
@@ -0,0 +1,28 @@
+var EventEmitter = require('./event').EventEmitter;
+
+var em = new EventEmitter();
+em.once('test', function() {
+	console.log('test once');
+});
+em.on('test', function() {
+	console.log('test multiple');
+});
+
+var Foo = function() {
+	this.x = 30;
+};
+Foo.prototype.printX = function() {
+	console.log(this.x);
+};
+var foo = new Foo();
+
+var callback = function(event) {
+	console.log(this);
+};
+em.on('test', foo.printX, foo);
+em.on('test', callback, em);
+em.on('test', callback, this);
+em.off('test', callback, this);
+
+em.trigger('test');
+em.trigger('test');
---
+++ FrontEnd/templates/event.js
@@ -0,0 +1,65 @@
+var event = event || {};
+
+(function(event) {
+
+	var EventEmitter = function() {
+		this.listeners = {};
+	};
+	EventEmitter.prototype.on = function(type, callback, context) {
+		this.listeners[type] = this.listeners[type] || [];
+		this.listeners[type].push({
+			callback: callback,
+			context: context || this
+		});
+	};
+	EventEmitter.prototype.once = function(type, callback, context) {
+		this.listeners[type] = this.listeners[type] || [];
+		this.listeners[type].push({
+			callback: callback,
+			context: context || this,
+			once: true
+		});
+	};
+	EventEmitter.prototype.off = function(type, callback, context) {
+		if (!type) {
+			this.listeners = {};
+		} else if (this.listeners[type] instanceof Array) {
+			if (callback) {
+				this.listeners[type] = this.listeners[type].filter(function(listener) {
+					var sameCallbacks = listener.callback === callback;
+					var sameContexts = context ? listener.context === context : true;
+					return !(sameCallbacks && sameContexts);
+				});
+			} else {
+				this.listeners[type] = [];
+			}
+		}
+	};
+	EventEmitter.prototype.trigger = function(event) {
+		if (typeof(event) === 'string') {
+			event = { type: event };
+		}
+		if (!event.target) {
+			event.target = this;
+		}
+		if (this.listeners[event.type] instanceof Array) {
+			var hasExpiredListeners = false;
+			this.listeners[event.type].forEach(function(listener) {
+				listener.callback.call(listener.context, event);
+				hasExpiredListeners = listener.once || hasExpiredListeners;
+			});
+			if (hasExpiredListeners) {
+				this.listeners[event.type] = this.listeners[event.type].filter(function(listener) {
+					return !listener.once;
+				});
+			}
+		}
+	};
+
+	event.EventEmitter = EventEmitter;
+
+	if (module) {
+		module.exports = event;
+	}
+
+})(event);
