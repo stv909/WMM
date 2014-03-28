2e6b55207730c7441ef45d4661cc6d61bf38d4fd
shadeglare <shadeglare@gmail.com>
2014-03-28 04:46:25 -0400

    added actions and gags data

--- FrontEnd/MultMessenger_code/data.js
+++ FrontEnd/MultMessenger_code/data.js
@@ -1,6 +1,7 @@
 var data = data || {};
 
 (function(data) {
+	
 	var MoodCollection = [
 		{ value: 'calm', icon: ":-|" }, 
 		{ value: "angry", icon: ">:(" }, 
@@ -9,5 +10,34 @@ var data = data || {};
 		{ value: "amaze", icon: ":-0" }
 	];
 	
+	var GagCollection = [
+		{ value: 'laugh', text: 'смеюсь' },
+		{ value: 'facepalm', text: 'сокрушаюсь' }, 
+		{ value: 'confusion', text: 'смущаюсь' },
+		{ value: 'cry', text: 'плачу' },
+		{ value: 'dance', text: 'танцую' },
+		{ value: 'exaltation', text: 'радуюсь' }, 
+		{ value: 'party', text: 'праздную' }, 
+		{ value: 'puke', text: 'болею' }, 
+		{ value: 'razz', text: 'дразнюсь' },
+		{ value: 'cake1left', text: 'кидаюсь тортом влево' },
+		{ value: 'cake1right', text: 'кидаюсь тортом вправо' }
+	];
+	
+	var ActionCollection = [
+		{ value: 'Idle', text: 'бездействую' }, 
+		{ value: 'point', text: 'указываю' },
+		{ value: 'time', text: 'тороплюсь' }, 
+		{ value: 'rulez', text: 'одобряю', },
+		{ value: 'sucks', text: 'осуждаю' }, 
+		{ value: 'applaud', text: 'аплодирую' }, 
+		{ value: 'fool', text: 'возмущаюсь' }, 
+		{ value: 'hi', text: 'приветствую' }, 
+		{ value: 'sun', text: 'сияю' }
+	];
+	
 	data.MoodCollection = MoodCollection;
+	data.GagCollection = GagCollection;
+	data.ActionCollection = ActionCollection;
+	
 })(data);
