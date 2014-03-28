ae1d5040ef88f8f3ca586b08f0da95834a866889
shdeglare <shadeglare@gmail.com>
2014-03-28 05:39:06 -0400

    generalized film text item dialog views

--- FrontEnd/MultMessenger_code/messenger.views.pages.EditPageView.js
+++ FrontEnd/MultMessenger_code/messenger.views.pages.EditPageView.js
@@ -75,8 +75,14 @@
 				analytics.send('editor', 'update_cancel');
 			});
 			
-			this.elem.getElementsByClassName('test')[0].addEventListener('click', function() {
+			this.elem.getElementsByClassName('test1')[0].addEventListener('click', function() {
 				self.moodsDialogView.show();
+			});
+			this.elem.getElementsByClassName('test2')[0].addEventListener('click', function() {
+				self.gagsDialogView.show();	
+			});
+			this.elem.getElementsByClassName('test3')[0].addEventListener('click', function() {
+				self.actionsDialogView.show();
 			});
 		}
 		
@@ -201,22 +207,42 @@
 		return FilmTextView;
 	})(abyss.View);
 	
+	var ItemView = (function(base) {
+		eve.extend(ItemView, base);
+		
+		function ItemView(model) {
+			base.apply(this, arguments);
+			this.selected = false;
+			this.model = model;
+		}
+		
+		ItemView.prototype.select = function() {
+			this.elem.classList.add('chosen');
+			this.elem.classList.remove('normal');
+			this.selected = true;
+		};
+		ItemView.prototype.deselect = function() {
+			this.elem.classList.remove('chosen');
+			this.elem.classList.add('normal');
+			this.selected = false;
+		};
+		
+		return ItemView;
+	})(abyss.View);
+	
 	var MoodItemView = (function(base) {
 		eve.extend(MoodItemView, base);
 		
-		function MoodItemView(mood) {
+		function MoodItemView(model) {
 			base.apply(this, arguments);
 			var self = this;
 			
 			this.elem = template.create('mood-item-template', { className: 'mood-item' });
-			this.elem.classList.add('normal');
 			this.iconElem = this.elem.getElementsByClassName('icon')[0];
 			
-			this.iconElem.textContent = mood.icon;
-			this.iconElem.classList.add(mood.value);
+			this.iconElem.textContent = model.icon;
+			this.iconElem.classList.add(model.value);
 			
-			this.model = mood;
-			this.selected = false;
 			this.deselect();
 			
 			var elemClickListener = function(event) {
@@ -232,9 +258,7 @@
 		}
 		
 		MoodItemView.prototype.select = function(silent) {
-			this.elem.classList.add('chosen');
-			this.elem.classList.remove('normal');
-			this.selected = true;
+			base.prototype.select.apply(this, arguments);
 			if (!silent) {
 				this.trigger({
 					type: 'select:mood',
@@ -242,14 +266,81 @@
 				});
 			}
 		};
-		MoodItemView.prototype.deselect = function() {
-			this.elem.classList.remove('chosen');
-			this.elem.classList.add('normal');
-			this.selected = false;
-		};
 		
 		return MoodItemView;
-	})(abyss.View);
+	})(ItemView);
+	
+	var GagItemView = (function(base) {
+		eve.extend(GagItemView, base);
+		
+		function GagItemView(model) {
+			base.apply(this, arguments);
+			var self = this;
+			
+			this.elem = template.create('gag-item-template', { className: 'gag-item' });
+			
+			this.deselect();
+			
+			var elemClickListener = function(event) {
+				if (!self.selected) {
+					self.select();
+				}
+			};
+			
+			this.elem.addEventListener('click', elemClickListener);
+			this.once('dispose', function() {
+				self.elem.removeEventListener('click', elemClickListener);
+			});
+		}
+		
+		GagItemView.prototype.select = function(silent) {
+			base.prototype.select.apply(this, arguments);
+			if (!silent) {
+				this.trigger({
+					type: 'select:gag',
+					gag: this.model
+				});
+			}
+		};
+		
+		return GagItemView;
+	})(ItemView);
+	
+	var ActionItemView = (function(base) {
+		eve.extend(ActionItemView, base);
+		
+		function ActionItemView(model) {
+			base.apply(this, arguments);
+			var self = this;
+			
+			this.elem = template.create('action-item-template', { className: 'action-item' });
+			
+			this.deselect();
+			
+			var elemClickListener = function(event) {
+				if (!self.selected) {
+					self.select();
+				}
+			};
+			
+			this.elem.addEventListener('click', elemClickListener);
+			this.once('dispose', function() {
+				self.elem.removeEventListener('click', elemClickListener);
+			});
+		}
+		
+		ActionItemView.prototype.select = function(silent) {
+			base.prototype.select.apply(this, arguments);
+			if (!silent) {
+				this.trigger({
+					type: 'select:gag',
+					action: this.model
+				});
+			}
+		};
+		
+		return ActionItemView;
+	})(ItemView);
 	
 	var MoodsDialogView = (function(base) {
 		eve.extend(MoodsDialogView, base);
@@ -323,6 +414,18 @@
 			
 			this.dialogWindowElem = document.getElementById('gags-dialog');
 			this.crossElem = this.dialogWindowElem.getElementsByClassName('cross')[0];
+			this.contentElem = this.dialogWindowElem.getElementsByClassName('content')[0];
+			
+			this.gagItemViews = {};
+			this.gagItemViewSelectListener = function(event) {
+				var gag = event.gag;
+				self.trigger({
+					type: 'select:gag',
+					gag: gag
+				});
+				self.hide();
+			};
+			this.initializeGagItemViews();
 			
 			var crossElementClickListener = function(event) {
 				self.hide();	
@@ -334,6 +437,32 @@
 				self.crossElem.removeEventListener('click', crossElementClickListener);
 			});
 		}
+		
+		GagsDialogView.prototype.initializeGagItemViews = function() {
+			data.GagCollection.forEach(function(gag) {
+				this.addGagItemView(gag);
+			}, this);
+		};
+		GagsDialogView.prototype.addGagItemView = function(gag) {
+			var gagItemView = new GagItemView(gag);
+			gagItemView.attachTo(this.contentElem);
+			gagItemView.on('select:gag', this.gagItemViewSelectListener);
+			this.gagItemViews[gag.value] = gagItemView;
+		};
+		GagsDialogView.prototype.show = function(gagValue) {
+			base.prototype.show.apply(this, arguments);
+			Object.keys(this.gagItemViews).forEach(function(key) {
+				this.gagItemViews[key].deslect();	
+			}, this);
+			var gagItemView = this.gagItemViews[gagValue];
+			if (gagItemView) {
+				gagItemView.select(true);
+			}
+		};
+		GagsDialogView.prototype.hide = function() {
+			base.prototype.hide.apply(this, arguments);
+			this.off('select:gag');
+		};
 		
 		return GagsDialogView;
 	})(DialogView);
@@ -347,9 +476,21 @@
 			
 			this.dialogWindowElem = document.getElementById('actions-dialog');
 			this.crossElem = this.dialogWindowElem.getElementsByClassName('cross')[0];
+			this.contentElem = this.dialogWindowElem.getElementsByClassName('content')[0];
+			
+			this.actionItemViews = {};
+			this.actionItemViewSelectListener = function(event) {
+				var action = event.action;
+				self.trigger({
+					type: 'select:action',
+					action: action
+				});
+				self.hide();
+			};
+			this.initializeActionItemViews();
 			
 			var crossElementClickListener = function(event) {
-				self.hide();	
+				self.hide();
 			};
 			
 			this.crossElem.addEventListener('click', crossElementClickListener);
@@ -358,6 +499,32 @@
 				self.crossElem.removeEventListener('click', crossElementClickListener);
 			});
 		}
+		
+		ActionsDialogView.prototype.initializeActionItemViews = function() {
+			data.ActionCollection.forEach(function(action) {
+				this.addActionItemView(action);
+			}, this);
+		};
+		ActionsDialogView.prototype.addActionItemView = function(action) {
+			var actionItemView = new ActionItemView(action);
+			actionItemView.attachTo(this.contentElem);
+			actionItemView.on('select:action', this.actionItemViewSelectListener);
+			this.actionItemViews[action.value] = actionItemView;
+		};
+		ActionsDialogView.prototype.show = function(actionValue) {
+			base.prototype.show.apply(this, arguments);
+			Object.keys(this.actionItemViews).forEach(function(key) {
+				this.actionItemViews[key].deselect();
+			}, this);
+			var actionItemView = this.actionItemViews[actionValue];
+			if (actionItemView) {
+				actionItemView.select(true);
+			}
+		};
+		ActionsDialogView.prototype.hide = function() {
+			base.prototype.hide.apply(this, arguments);
+			this.off('select:action');
+		};
 		
 		return ActionsDialogView;
 	})(DialogView);
