f365d7000d98830dfbc1294b31c96d0c49a333e4
shadeglare <shadeglare@gmail.com>
2014-04-15 11:25:39 +0400

    fixed wrong model usage

--- FrontEnd/MultMessenger_code/application.js
+++ FrontEnd/MultMessenger_code/application.js
@@ -699,7 +699,7 @@ window.onload = function() {
 		this.on('click:send', function(event) {
 			var toContact = self.chatRepository.contact;
 			var fromContact = self.contactRepository.owner;
-			var chatMessage = new messenger.repository.ChatMessageModel();
+			var chatMessage = new messenger.data.ChatMessageModel();
 			chatMessage.set({
 				id: eye.uuid(),
 				content: event.text,
