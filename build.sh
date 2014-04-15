2e5b6c2d2b600c558cdc90afb5d1d990e833e64d
shadeglare <shadeglare@gmail.com>
2014-04-15 17:19:42 +0400

    fixed build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf *
 cd ..
 cp -r MultMessenger_code/* MultMessenger_content
 cd MultMessenger_content
-perl -pi -e "s/settings.version = 'dev'/settings.version = 'content'/g" settings.js
+perl -pi -e "s/Settings.version = 'dev'/Settings.version = 'content'/g" messenger.settings.js
 cd ..
 
 cd MultMessenger_release/
@@ -13,13 +13,13 @@ rm -rf *
 cd ..
 cp -r MultMessenger_code/* MultMessenger_release
 cd MultMessenger_release
-perl -pi -e "s/settings.version = 'dev'/settings.version = 'release'/g" settings.js
+perl -pi -e "s/Settings.version = 'dev'/Settings.version = 'release'/g" messenger.settings.js
 cd ..
 
-cd MultMessenger_english/
-rm -rf *
-cd ..
-cp -r MultMessenger_code/* MultMessenger_english
-cd MultMessenger_english
-perl -pi -e "s/settings.version = 'dev'/settings.version = 'english'/g" settings.js
-cd ..
+#cd MultMessenger_english/
+#rm -rf *
+#cd ..
+#cp -r MultMessenger_code/* MultMessenger_english
+#cd MultMessenger_english
+#perl -pi -e "s/settings.version = 'dev'/settings.version = 'english'/g" settings.js
+#cd ..
