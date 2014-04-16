a2e163217d5e263bbcf4c8d9546d2c2940a041c7
shadeglare <shadeglare@gmail.com>
2014-04-16 13:54:22 +0400

    updated build script

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cd ..
 cp -r MultMessenger_code/* MultMessenger_content
 cd MultMessenger_content
 perl -pi -e "s/Settings.version = 'dev'/Settings.version = 'content'/g" messenger.Settings.js
+perl -pi -e "s/version = 'dev'/version = 'content'/g" messenger.Settings.ts
 cd ..
 
 cd MultMessenger_release/
@@ -14,6 +15,7 @@ cd ..
 cp -r MultMessenger_code/* MultMessenger_release
 cd MultMessenger_release
 perl -pi -e "s/Settings.version = 'dev'/Settings.version = 'release'/g" messenger.Settings.js
+perl -pi -e "s/version = 'dev'/version = 'release'/g" messenger.Settings.js
 cd ..
 
 #cd MultMessenger_english/
